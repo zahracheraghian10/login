8f204d0a09a0290e7e33262ec505a76e4e7eecb4
unknown <you@example.com>
2023-08-27 10:01:57 -0700

    first commit

--- static/assets/js/login.js
+++ static/assets/js/login.js
@@ -1,21 +1,21 @@
-// const wrapper =document.querySelector('.wrapper');
-// const loginlink =document.querySelector('.login-link');
-// const registerlink =document.querySelector('.register-link');
-// const form=document.querySelector('form')
-// const btn=document.querySelector('btn');
-// registerlink.addEventListener('click', ()=>{
-//     wrapper.classList.add('active');
+const wrapper =document.querySelector('.wrapper');
+const loginlink =document.querySelector('.login-link');
+const registerlink =document.querySelector('.register-link');
+const form=document.querySelector('form')
+const btn=document.querySelector('btn');
+registerlink.addEventListener('click', ()=>{
+    wrapper.classList.add('active');
 
-// });
-// loginlink.addEventListener('click', ()=>{
-//     wrapper.classList.remove('active');
+});
+loginlink.addEventListener('click', ()=>{
+    wrapper.classList.remove('active');
 
-// });
-// const username=document.querySelector('.username');
-// username.addEventListener('keydown',()=>{
-//     if(username.value.length==8){
-//     btn.classList.add('btn2');
-//     }
-//     console.log(username.innerHTML)
+});
+const username=document.querySelector('.username');
+username.addEventListener('keydown',()=>{
+    if(username.value.length==8){
+    btn.classList.add('btn2');
+    }
+    console.log(username.innerHTML)
     
-// })
+})
