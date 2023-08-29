64d81b2c448c53605f6f18e75f9b6e2026718c77
Alireza <alireza.shahnazarii@gmail.com>
2023-08-29 09:14:23 -0700

    fix bugs of login page

--- static/assets/js/login.js
+++ static/assets/js/login.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const registerlink = document.querySelector('.register-link');
 const form = document.querySelector('form');
-const btn = document.querySelector('.btn');
+const btn = document.querySelectorAll('.btn');
 registerlink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
@@ -12,8 +12,13 @@ loginlink.addEventListener('click', () => {
 
 const username = document.querySelectorAll('.username');
 username.forEach((inp,index) => {
-    inp.onchange = () => {
-        console.log("input number " + index + ": ", inp.value);
+    inp.oninput = () => {
+        console.log(inp.value.length);
+        if(inp.value.length >= 8) {
+            btn[index].classList.add('btn2');
+        }else {
+            btn[index].classList.remove('btn2');
+        }
     }
 });
 
